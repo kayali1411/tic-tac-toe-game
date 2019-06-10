738f285d49400a706b7d0698d624e8251c1c7780
ahmad.kayali <ahmad.kayali@sultan-center.com>
2019-06-10 15:47:19 +0300

    handle win and draw cases

--- src/Components/Game.js
+++ src/Components/Game.js
@@ -6,25 +6,43 @@ import Control from './Control';
 const Game = () => {
 
     const { currentPlayer, playerMoves } = useContext(GameContext);
-    const [ winner, setWinner ] = useState('');
+    const [ result, setResult ] = useState({ endGame: '', winner: '', player: '' });
+
 
     const checkWinner = (player) => {
         const winnerCode = ['ABC', 'DEF', 'GHI', 'ADG', 'BEH', 'CFI', 'AEI', 'CEG'];
         const isWinner = winnerCode.includes(playerMoves[player]);
+
         if(isWinner) {
-            setWinner(player);
+            setResult({
+                endGame: true,
+                winner: true,
+                player
+            });
+        }
+
+        if((playerMoves.playerOne.length + playerMoves.playerTwo.length) === 9) {
+            setResult({
+                endGame: true,
+                winner: false,
+                player: 'Draw'
+            });
         }
     };
 
     useEffect(() => {
-        const prePlayer = currentPlayer === 'playerOne' ? 'playerTwo' : 'playerOne';
-        checkWinner(prePlayer);
+        if(!!currentPlayer) {
+            const prePlayer = currentPlayer === 'playerOne' ? 'playerTwo' : 'playerOne';
+            checkWinner(prePlayer);
+        } else {
+            setResult({ endGame: '', winner: '', player: '' });
+        }
     }, [currentPlayer]);
 
     return (
         <>
             <h1>{currentPlayer}</h1>
-            <h2>{winner}</h2>
+            <h2>{result.endGame && result.player}</h2>
             <Blocks />
             <Control />
         </>
